fe5c09fcc5c68e9be221d3827f77b4de10e9f7b3
DLDelfin <122779293+DLDelfin@users.noreply.github.com>
2023-01-23 11:46:16 +0800

    Add files via upload

---
+++ prelims/node-example.js
@@ -0,0 +1,19 @@
+// Name: Dayson L. Delfin
+// Module 3
+// Week 2 and 3: Basics of NodeJS
+
+//First Program
+console.log("Welcome to WCServer!!");
+
+//Basics of NodeJS
+
+//Creating my first function
+function Display(x) {
+    console.log(x);
+}
+Display(200);
+
+
+
+
+
