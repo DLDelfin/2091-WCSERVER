e87c1abf116c9928cfa1c78d93118ae529c3fdb4
DLDelfin <122779293+DLDelfin@users.noreply.github.com>
2023-01-30 13:25:08 +0800

    1 file modified

--- PRELIMS/Prelim Lab Activities/Prelim-Lab-Activity-3/server.js
+++ PRELIMS/Prelim Lab Activities/Prelim-Lab-Activity-3/server.js
@@ -10,18 +10,18 @@ var server = http.createServer(function (req, res) {
   } else if (req.url == '/about') {
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.write(
-      '<html><h1>This is Student Page.</h1><body><p>Hello! My name is Dayson Delfin of WD - 201</p></body></html>'
+      '<html><h1>This is the About Page.</h1><body><p>Hello Dayson Delfin. This activity will teach on how to deal with a simple server and local modules in Node.js</p></body></html>'
     );
     res.end();
   } else if (req.url == '/contact') {
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.write(
-      '<html><h1>This is Admin Page.</h1><body><p>Hello! My name is Dayson L. Delfin of WD - 201</p></body></html>'
+      '<html><h1>This is contact Page.</h1><body><p>Dayson L. Delfin, if you want additional details about this activity go to this site://www.tutorials.com</p></body></html>'
     );
   } else if (req.url == '/gallery') {
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.write(
-      '<html><h1>This is Admin Page.</h1><body><p>Hello! My name is Dayson L. Delfin of WD - 201</p></body></html>'
+      '<html><h1>This is the Galley page.</h1><body><p>Hello! My name is Dayson L. Delfin of WD - 201</p></body></html>'
     );
     res.end();
   } else res.end('Invalid Request!');
