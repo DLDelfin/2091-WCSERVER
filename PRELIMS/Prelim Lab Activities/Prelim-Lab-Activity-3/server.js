86c8264cccc798010cf38be9cb64871a89b9549a
DLDelfin <122779293+DLDelfin@users.noreply.github.com>
2023-01-30 13:19:48 +0800

    1 file modified

--- PRELIMS/Prelim Lab Activities/Prelim-Lab-Activity-3/server.js
+++ PRELIMS/Prelim Lab Activities/Prelim-Lab-Activity-3/server.js
@@ -4,16 +4,21 @@ var server = http.createServer(function (req, res) {
   if (req.url == '/') {
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.write(
-      '<html><h1>This is Home Page.</h1><body><p>Hello! My name is Dayson L. Delfin of WD - 201/p></body></html>'
+      '<html><h1> Welcome to my Node.js Application</h1><body><p> Welcome Dayson L. Delfin. This is an activity about basics of Node.js/p></body></html>'
     );
     res.end();
-  } else if (req.url == '/student') {
+  } else if (req.url == '/about') {
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.write(
       '<html><h1>This is Student Page.</h1><body><p>Hello! My name is Dayson Delfin of WD - 201</p></body></html>'
     );
     res.end();
-  } else if (req.url == '/admin') {
+  } else if (req.url == '/contact') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write(
+      '<html><h1>This is Admin Page.</h1><body><p>Hello! My name is Dayson L. Delfin of WD - 201</p></body></html>'
+    );
+  } else if (req.url == '/gallery') {
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.write(
       '<html><h1>This is Admin Page.</h1><body><p>Hello! My name is Dayson L. Delfin of WD - 201</p></body></html>'
