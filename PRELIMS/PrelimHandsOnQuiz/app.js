dce4b04c910a12061094b6bd8b3511e2d79d664c
DLDelfin <122779293+DLDelfin@users.noreply.github.com>
2023-01-30 21:49:04 +0800

    1 file modified

---
+++ PRELIMS/PrelimHandsOnQuiz/app.js
@@ -0,0 +1,11 @@
+var calcul = require('./calculation.js');
+
+var hours = 4;
+var days = 6;
+var rate = 300;
+var tax = 0.1;
+var sss = 1200;
+var pagbig = 300;
+var phealth = 400;
+
+console.log('The Gross Income is: ');
