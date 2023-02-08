eb5671f5766b82a95a076c0e9390b56a8334854c
DLDelfin <122779293+DLDelfin@users.noreply.github.com>
2023-02-08 16:04:59 +0800

    2 files added

---
+++ PRELIMS/PrelimExam/app.js
@@ -0,0 +1,5 @@
+var { greet, congratulations, farewell } = require('./message.js');
+
+console.log(greet('Dayson L. Delfin'));
+console.log(congratulations('Dayson L. Delfin'));
+console.log(farewell('Dayson L. Delfin'));
