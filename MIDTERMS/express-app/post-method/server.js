1bb82f1b4253bb14cec97131275331a4ce045177
DLDelfin <122779293+DLDelfin@users.noreply.github.com>
2023-02-27 13:25:46 +0800

    5 files added, 1 deleted

---
+++ MIDTERMS/express-app/post-method/server.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var app = express();
+
+//require body-parser for this method
+var bodyParser = require('body-parser');
+
+//Create application/x-www-form-urlencoded paser
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+app.get('/', function (req, res) {
+res.sendFile(__dirname + '/' + 'index.html');
+});
+
+app.post('/process_post', urlencodedParser, function (req, res) {
+    response = {
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+};
+console.log(response);
+res.end(JSON.stringify(response));
+});
+
+//Setting the listener to ENV PORT info
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+console.log(`listening on port ${port}`);
+});
