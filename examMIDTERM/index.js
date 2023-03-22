c444690c0db0888703a3c9af0d5f7540486059ba
DLDelfin <122779293+DLDelfin@users.noreply.github.com>
2023-03-22 16:09:36 +0800

    1 file added

--- examMIDTERM/index.js
+++ examMIDTERM/index.js
@@ -1,24 +1,29 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('My New App!');
+const dishes = [
+  { type: 'sisig', province: 'pampanga', price: 220 },
+  { type: 'salpicao', province: 'quezon', price: 180 },
+  { type: 'bagnet', province: 'ilocos', price: 370 },
+];
+
+app.get('/dishes', (req, res) => {
+  res.json(dishes);
 });
 
-app.get('/api/heroes', (req, res) => {
-  res.send(['Superman', 'Iron Man', 'Batman', 'Hulk']);
+app.get('/dishes/:type', (req, res) => {
+  const dish = dishes.find((d) => d.type === req.params.type);
+  if (!dish) {
+    res.status(404).json({ error: 'Record not found' });
+  } else {
+    res.json(dish);
+  }
 });
 
-app.get('/api/heroes/:id', (req, res) => {
-  res.send(req.params.id);
+app.use((req, res) => {
+  res.status(404).json({ error: 'Record not found' });
 });
 
-app.get('/api/heroes/:title/:publisher', (req, res) => {
-  res.send(req.params);
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
 });
-
-app.get('/api/heroes/:title/:publisher', (req, res) => {
-  res.send([req.params, req.query]);
-});
-
-app.listen(3000, () => console.log('Listening on port 3000'));
